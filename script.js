c93f0e8b15711e18176546bfda9550f316afafd8
sugarylipss <iyeravisha@gmail.com>
2026-09-10 22:16:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ beginButton.addEventListener("click", function() {
     intro.style.display = "none";
     sorting.style.display = "flex";
 });
+
+const houseButtons = document.querySelectorAll(".houses button");
+
+houseButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+
+        const house = button.textContent;
+
+        alert("The Sorting Hat has chosen: " + house);
+
+    });
+});
